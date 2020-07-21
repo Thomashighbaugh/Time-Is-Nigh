4a6679f648482c491ab9ae855e8c8c552831f459
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-07-21 16:13:33 -0700

    Committed by tlh

---
+++ build/precache-manifest.659fb5c07feaa771b370f12d89c855b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c594286ff7e5f8cd141223bfba71f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04e33c491795e6042c3d",
+    "url": "/static/js/2.5412f27c.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.5412f27c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f48838ce79b6d01877",
+    "url": "/static/js/main.9b8387d4.chunk.js"
+  },
+  {
+    "revision": "224bde3e02d8e56245b4",
+    "url": "/static/js/runtime-main.4aa8a949.js"
+  },
+  {
+    "revision": "b770a68f63b23e78416171e8b276190d",
+    "url": "/static/media/logo.b770a68f.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50cece9ab92e07fe10f18d2e05caca3e.js"
+  "/precache-manifest.659fb5c07feaa771b370f12d89c855b1.js"
 );
 
 self.addEventListener('message', (event) => {
